0d57b965c877e36af1fbf5c66cf4d63756c5ecab
S4ma3l <66357485+S4ma3l@users.noreply.github.com>
2022-11-21 19:27:29 +0530

    Update primeornot.py

--- primeornot.py
+++ primeornot.py
@@ -1,10 +1,13 @@
+#Program to find the nth prime number
+
+
 def primeOrNot(num):
 
     for i in range(2,num):
         if num%i==0:
             return False
     return True
-#print(primeOrNot(int(input("Enter a number to check whether it's prime or not"))))
+
 def returnnthPrime(nthNumber):
     count = 0
     numToCheck =1
@@ -15,4 +18,4 @@ def returnnthPrime(nthNumber):
     return numToCheck
 
 
-print("Nth: ", returnnthPrime(int(input("Enter a number"))))
+print("Nth prime number is: ", returnnthPrime(int(input("Enter a number"))))
