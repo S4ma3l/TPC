54b1b4ad93dbf3d1aecdd594a18b34393dc1ea02
S4ma3l <66357485+S4ma3l@users.noreply.github.com>
2022-11-21 19:02:40 +0530

    Update GP_Series.py

--- GP_Series.py
+++ GP_Series.py
@@ -1,3 +1,6 @@
+#Question - In a series of numbers all odd numbers are replaced by numbers in a series of Geometric progression with formula (a*(r**(n-1))) here a = 1, and r=2 for #first series. All even positions are replaced by another geometric progression series of the formula (a*(r**(n-1))) here a =1 and r=3
+#Find the nth term in the series?
+
 num = int(input("Enter value of n"))
 a=1
 r1=2
